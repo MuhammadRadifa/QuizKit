f4749260819897e11274a9e74c12277df3dae82f
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-15 16:08:09 +0800

    initial commit

---
+++ app/src/main/java/com/example/quizkit/data/OnBoarding.kt
@@ -0,0 +1,12 @@
+package com.example.quizkit.data
+
+data class OnBoarding(
+    val title:String = "",
+    val description:String = ""
+)
+
+val onBoardingData = mapOf<Int,OnBoarding>(
+    0 to OnBoarding("",""),
+    1 to OnBoarding("",""),
+    2 to OnBoarding("","")
+)
