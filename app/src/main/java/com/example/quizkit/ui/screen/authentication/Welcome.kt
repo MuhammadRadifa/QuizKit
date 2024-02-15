57382916059bf65fedec528e04ad3325fcca7f1b
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-15 15:45:47 +0800

    add Welcome Screen

---
+++ app/src/main/java/com/example/quizkit/ui/screen/authentication/Welcome.kt
@@ -0,0 +1,100 @@
+package com.example.quizkit.ui.screen.authentication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quizkit.R
+
+@Composable
+fun Welcome(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary_purple)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.welcome), contentDescription = "Welcome")
+        Column(
+            Modifier
+                .padding(20.dp)
+                .clip(shape = RoundedCornerShape(10))
+                .background(color = Color.White)
+        ) {
+            Column(
+                Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                Text(
+                    text = "Welcome",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = colorResource(id = R.color.text_gray),
+                    textAlign = TextAlign.Center,
+                    text = "Login or create an account to take quiz, take part in challenge, and more."
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.primary_purple)
+                    )
+                ) {
+                    Text(text = "Login", fontSize = 18.sp)
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.primary_blue),
+                        contentColor = colorResource(id = R.color.primary_purple)
+                    )
+                ) {
+                    Text(text = "Create an account", fontSize = 18.sp)
+                }
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WelcomePreview(){
+    Welcome()
+}
---
+++ app/src/main/java/com/example/quizkit/ui/screen/onboarding/OnBoarding.kt
@@ -0,0 +1,16 @@
+package com.example.quizkit.ui.screen.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OnBoarding(){
+
+}
+
+@Preview
+@Composable
+fun OnBoardingPreview(){
+    OnBoarding()
+}
+
