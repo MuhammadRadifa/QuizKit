427ea5fae5c1235867dbf59452125100161ecdc5
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-15 16:07:25 +0800

    add font

--- app/src/main/java/com/example/quizkit/ui/theme/Type.kt
+++ app/src/main/java/com/example/quizkit/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.example.quizkit.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.quizkit.R
+
+val monserratFamily = FontFamily(
+    Font(R.font.light,FontWeight.Light),
+    Font(R.font.regular,FontWeight.Normal),
+    Font(R.font.medium,FontWeight.Medium),
+    Font(R.font.semibold,FontWeight.SemiBold),
+    Font(R.font.bold,FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = monserratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
