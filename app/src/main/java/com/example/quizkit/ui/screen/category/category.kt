e39eedfe1ffba4d47b175a4e11c66f99c64d7d3c
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-17 16:10:19 +0800

    refactor filename

--- app/src/main/java/com/example/quizkit/ui/screen/category/category.kt
+++ app/src/main/java/com/example/quizkit/ui/screen/category/category.kt
@@ -1,8 +1,6 @@
 package com.example.quizkit.ui.screen.category
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -23,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
